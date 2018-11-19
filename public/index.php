6ccc913fd6b238c7ee1849351e98facda69f6fc1
Cristopher Castro <cristophernic@live.cl>
2018-11-19 17:07:44 -0300

    Mensajes de error, sin dependencias

--- public/index.php
+++ public/index.php
@@ -1,17 +1,15 @@
 <?php
 
-/**
- * A super-simple user-authentication solution, embedded into a small framework.
- *
- * HUGE
- *
- * @link https://github.com/panique/huge
- * @license http://opensource.org/licenses/MIT MIT License
- */
-
-// auto-loading the classes (currently only from application/libs) via Composer's PSR-4 auto-loader
-// later it might be useful to use a namespace here, but for now let's keep it as simple as possible
-require '../vendor/autoload.php';
-
-// start our application
-new Application();
+try {
+    if (! @include_once( '../vendor/autoload.php' ))
+        throw new Exception ('Composer no ha terminado de instalar las dependencias');
+        // or 
+        if (!file_exists('../vendor/autoload.php' ))
+            throw new Exception ('Composer no ha terminado de instalar las dependencias');
+        else
+            require '../vendor/autoload.php';
+            new Application();
+        }
+catch(Exception $e) {    
+    echo 'Composer no ha terminado de instalar las dependencias';
+}
